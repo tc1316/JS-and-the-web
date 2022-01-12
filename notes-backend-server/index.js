ca0aeb0feb6c7e55197c287e93f600a96c1820c8
BoneSponge <50413143+BoneSponge@users.noreply.github.com>
2022-01-12 15:47:51 +0000

    Moved backend server inside

---
+++ notes-backend-server/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const cors = require('cors')
+const app = express();
+const PORT = 3000;
+
+let notes = [
+  'This note is coming from the server'
+];
+
+app.use(express.json());
+
+app.use(
+  cors({
+  origin: '*',
+}));
+
+
+app.get('/notes', (_req, res) => {
+  res.set({
+    "Access-Control-Allow-Origin" : "*", 
+"Access-Control-Allow-Credentials" : true 
+});  res.send(JSON.stringify(notes))
+});
+
+app.post('/notes', (req, res) => {
+  res.set({
+    "Access-Control-Allow-Origin" : "*", 
+"Access-Control-Allow-Credentials" : true 
+  })
+  notes.push(req.body.content)
+  res.send(JSON.stringify(notes));
+});
+
+app.delete('/notes', (req, res) => {
+  notes = [];
+  res.send(JSON.stringify(notes))
+});
+
+app.listen(PORT);
---
+++ notes-backend-server/notesApi.test.js
@@ -0,0 +1,27 @@
+const NotesApi = require('./notesApi')
+
+require('jest-fetch-mock').enableMocks()
+
+describe('Notes class', () => {
+  it("calls fetch and load notes", async () => {
+    const api = new NotesApi();
+    fetch.mockResponseOnce(JSON.stringify(
+      ['This note is coming from the server'])
+    )
+
+    api.loadNotes((notes)=> {
+      expect(notes).toEqual(['This note is coming from the server'])})
+  })
+
+  it('calls fetch to create new notes', async () => {
+    const api = new NotesApi();
+    let payload = fetch.mockResponseOnce(JSON.stringify({ "content": "Remember to reflect on my progress this week!" }))
+
+    api.createNote(payload,(note) => {
+      expect(note).toEqual({ "content": "Remember to reflect on my progress this week!" })
+    } )
+
+  })
+
+
+})
